ca07399ed7afbe504de5a290528033b8ac80b28a
alaadin2005 <alaa_din_99@yahoo.com>
2025-12-29 10:49:55 +0300

    Create mariadb.sh

---
+++ mariadb.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+# Switch to root user
+sudo -i
+
+rm -rf /etc/yum.repos.d/*
+cat << EOF > /etc/yum.repos.d/app.repo
+[appstream]
+name=CentOS Stream 9 - AppStream
+baseurl=http://mirror.stream.centos.org/9-stream/AppStream/x86_64/os/
+enabled=1
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-centosofficial
+
+[baseos]
+name=CentOS Stream 9 - BaseOS
+baseurl=http://mirror.stream.centos.org/9-stream/BaseOS/x86_64/os/
+enabled=1
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-centosofficial
+
+[crb]
+name = CentOS Stream 9 -CRB
+baseurl = http://mirror.stream.centos.org/9-stream/CRB/x86_64/os/
+enabled = 1
+gpgcheck = 1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-centosofficial
+
+EOF
+
+# Update package list and install EPEL release
+echo "Installing EPEL release..."
+yum install epel-release -y
+
+# Install MariaDB and Git
+echo "Installing MariaDB and Git..."
+yum install git mariadb-server -y
+
+# Start and enable MariaDB service
+echo "Starting and enabling MariaDB service..."
+systemctl start mariadb
+systemctl enable mariadb
+
+# Run mysql_secure_installation script automatically
+echo "Securing MariaDB..."
+expect <<EOF
+spawn mysql_secure_installation
+expect "Enter current password for root (enter for none):"
+send "\r"
+expect "Set root password? \[Y/n\]"
+send "y\r"
+expect "New password:"
+send "admin123\r"
+expect "Re-enter new password:"
+send "admin123\r"
+expect "Remove anonymous users? \[Y/n\]"
+send "y\r"
+expect "Disallow root login remotely? \[Y/n\]"
+send "y\r"
+expect "Remove test database and access to it? \[Y/n\]"
+send "y\r"
+expect "Reload privilege tables now? \[Y/n\]"
+send "y\r"
+expect eof
+EOF
+
+# Configure MariaDB: Create database and user
+echo "Configuring MariaDB database and user..."
+mysql -u root -padmin123 <<MYSQL_SCRIPT
+CREATE DATABASE accounts;
+GRANT ALL PRIVILEGES ON accounts.* TO 'admin'@'%' IDENTIFIED BY 'admin123';
+FLUSH PRIVILEGES;
+MYSQL_SCRIPT
+
+# Download source code and initialize database
+echo "Cloning source code and initializing database..."
+git clone -b main https://github.com/alaadin2005/vprofile-project.git
+cd vprofile-project
+mysql -u root -padmin123 accounts < src/main/resources/db_backup.sql
+
+# Restart MariaDB service
+echo "Restarting MariaDB service..."
+systemctl restart mariadb
+
+# Start and configure the firewall to allow access to MariaDB on port 3306
+echo "Configuring firewall..."
+systemctl start firewalld
+systemctl enable firewalld
+firewall-cmd --zone=public --add-port=3306/tcp --permanent
+firewall-cmd --reload
+
+echo "MariaDB setup is complete."
